cbbffdadb5af7fc5ac985d32c55bb656d0ef6f3b
Yuriy Taras <ytaras@softserveinc.com>
2017-02-16 11:28:21 +0200

    Fixed foreign keys

--- src/main/resources/db/migration/V20170215220326__Create_attempts_table.sql
+++ src/main/resources/db/migration/V20170215220326__Create_attempts_table.sql
@@ -1,10 +1,13 @@
 -- For H2 Database
 create table attempts (
   id bigserial not null primary key,
-  training_id bigint not null references trainings (id),
-  exercise_id bigint not null references trainings (id),
-  shooter_id bigint not null references trainings (id),
+  training_id bigint not null,
+  exercise_id bigint not null,
+  shooter_id bigint not null,
   result Decimal(5,2),
   created_at timestamp not null,
-  updated_at timestamp not null
+  updated_at timestamp not null,
+  constraint attempts_training_id foreign key (training_id) references trainings (id),
+  constraint attempts_shooter_id foreign key (shooter_id) references shooters (id),
+  constraint attempts_exercise_id foreign key (exercise_id) references exercises (id)
 )
